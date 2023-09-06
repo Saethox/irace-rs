fc8f3de0d6b5cfdc021f385b1e2015bcbe938912
Jonathan Wurth <jonathan.wurth@uni-a.de>
2023-09-07 01:03:44 +0200

    Add categorical names in example

--- examples/parameter_space.rs
+++ examples/parameter_space.rs
@@ -16,6 +16,7 @@ fn main() {
         .with_bool("no_local_search")
         .with_integer("population_size", 5, 64, false)
         .with_categorical("option", [Option1, Option2, Option3])
+        .with_categorical_names("option", ["yes", "no"])
         .with_nested(
             "0",
             ParamSpace::new().with_real("nested_parameter", 0.0, 1.0, false),
